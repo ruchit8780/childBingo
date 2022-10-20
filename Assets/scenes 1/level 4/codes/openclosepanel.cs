4e0b3d42e43b2b033f269f203ea624d4ea6f0d99
sanjana92 <sanjanaseelam.92@gmail.com>
2022-10-19 21:39:59 -0700

    week 8

---
+++ Assets/scenes 1/level 4/codes/openclosepanel.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class openclosepanel : MonoBehaviour
+{
+    public GameObject Panel;
+
+
+    public void OpenPanel()
+    {
+        if(Panel != null)
+        {
+            Panel.SetActive(true);
+        }
+    }    
+}
